e3654792e025e45d7cced076abfc11d55cb6b047
ndela-ntu <ntulilindelani4@gmail.com>
2025-01-11 18:40:40 +0200

    moved continue link/button

--- app/services/[id]/page.tsx
+++ app/services/[id]/page.tsx
@@ -20,8 +20,19 @@ export default async function Page({
   }
 
   return (
-    <div className="min-h-screen flex flex-col items-center space-y-2.5">
-      <h1 className="text-3xl text-cinereous font-bold">{service.name}</h1>
+    <div className="min-h-screen flex flex-col items-center space-y-2.5 p-2">
+      <div className="flex justify-between items-center w-full">
+        <h1 className="text-2xl text-cinereous font-bold underline">{service.name}</h1>
+        <Link
+          href={`/service-request/${service.id}`}
+          className="flex my-3 mx-1.5 px-2.5 py-1 bg-yaleBlue text-ghostWhite rounded-3xl hover:bg-cinereous"
+        >
+          <span>Continue</span>
+          <span>
+            <MoveRight />
+          </span>
+        </Link>
+      </div>
       <div className="aspect-square relative w-[75%] md:w-[50%] lg:w-[25%] border-8 border-ghostWhite">
         <Image
           src={service.categoryImageUrl}
@@ -32,15 +43,6 @@ export default async function Page({
         />
       </div>
       <div className="text-center md:w-[50%]">{service.description}</div>
-      <Link
-        href={`/service-request/${service.id}`}
-        className="flex my-3 mx-1.5 px-2.5 py-1 bg-yaleBlue text-ghostWhite rounded-3xl hover:bg-cinereous"
-      >
-        <span>Continue</span>
-        <span>
-          <MoveRight />
-        </span>
-      </Link>
     </div>
   );
 }
